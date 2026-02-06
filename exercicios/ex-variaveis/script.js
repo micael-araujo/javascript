f4e4c32c67515109d745c395ef91234cc52f285d
Micael Araujo <micaelsilva.dev@gmail.com>
2026-02-06 19:26:45 -0300

    criando meu primeiro exercicio de js

---
+++ exercicios/ex-variaveis/script.js
@@ -0,0 +1,18 @@
+var nome = 'Micael',
+    idade = 18,
+    comidaFavorita;
+    comidaFavorita = 'Pizza';
+    
+var peso,
+    altura,
+    Imc,
+    animalFavorito,
+    faculdade;
+
+    peso = 50.
+    altura = '1.71'
+    Imc = 23.0,
+    animalFavorito = 'cachorro',
+    faculdade = 'Análise e Desenvolvimento de Sistemas';
+
+console.log(nome, 'tem ', idade, 'anos e sua comida favorita é ', comidaFavorita, '. Ele pesa ', peso, 'kg e sua altura é ', altura, '. Calculando seu imc é igual a ', Imc, '. Ele tem como animal favorito o ', animalFavorito, ' e, por último, ele cursa ', faculdade)
