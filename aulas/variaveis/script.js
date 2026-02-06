cdf3c2e929b337ae826f7d3f3e0eec65bad7da42
Micael Araujo <micaelsilva.dev@gmail.com>
2026-02-06 17:52:57 -0300

    Iniciando os estudos de js

---
+++ aulas/variaveis/script.js
@@ -0,0 +1,24 @@
+var nome = 'Micael';
+  preco = 25;
+  totalComprado = 5;
+  totalPreco = totalComprado * preco;
+
+console.log(nome, 'comprou ', totalPreco, ' maçãs no mercado.')
+
+/*
+Variáveis não definidas
+
+var semDefinicao;
+console.log(semDefinicao)
+
+Ordem importa
+
+console.log(aindaNaoExiste)
+var aindaNaoExiste;
+*/
+
+// Variáveis podem mudar
+var time = 'Santos';
+console.log(time)
+var time = 'Vasco';
+console.log(time)
