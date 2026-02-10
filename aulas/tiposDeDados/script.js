2ff964e99a310d26707f04047770b7b62a126ad2
Micael Araujo <micaelsilva.dev@gmail.com>
2026-02-10 15:48:10 -0300

    Tipos de Dados em JavaScript

---
+++ aulas/tiposDeDados/script.js
@@ -0,0 +1,22 @@
+var nome = 'Micael';
+var idade = 18;
+console.log(typeof nome)
+console.log(typeof idade)
+
+var sobrenome = 'Araújo';
+
+console.log(nome + ' ' + sobrenome)
+
+var jogador = 'Pelé';
+var gols = 1000;
+var frase = jogador + ' ' + gols
+console.log(frase)
+console.log(typeof frase)
+
+var ano = '2026';
+var mes = 2;
+console.log(ano + mes)
+
+var frase1 = 'Esse é o \'melhor\' jogo';
+
+console.log(`Eu me chamo ${nome} e já fiz ${gols * 2} gols.`)
---
+++ exercicios/ex-tipoDeDados/script.js
@@ -0,0 +1,25 @@
+// Declare uma variável contendo uma string
+var palavra = 'teste';
+
+// Declare uma variável contendo um número dentro de uma string
+var n1 = '10';
+
+// Declare uma variável com a sua idade
+var idade = 18;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Micael',
+    sobrenome = 'Araújo',
+    nomeCompleto = `${nome} ${sobrenome}`;
+    console.log(nomeCompleto)
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time",
+    frase2 = `It\'\s time 2`;
+    console.log(frase)
+    console.log(frase2)
+
+// Verifique o tipo da variável que contém o seu nome
+var verificarTipoNome = typeof nome;
+console.log(verificarTipoNome)
