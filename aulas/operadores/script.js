292c14b4a4d7a7fb661f63478cbd3d4460b4938f
Micael Araujo <micaelsilva.dev@gmail.com>
2026-02-10 20:41:48 -0300

    Operadores

---
+++ aulas/operadores/script.js
@@ -0,0 +1,23 @@
+var soma = 5 + 5,
+    subtracao = 5 - 5,
+    multiplicacao = 5 * 5,
+    divisao = 5 / 5,
+    potencia = 5 ** 5,
+    modulo = 33 % 5;
+
+console.log(`5 + 5 = ${soma}`)
+console.log(`5 - 5 = ${subtracao}`)
+console.log(`5 x 5 = ${multiplicacao}`)
+console.log(`5 / 5 = ${divisao}`)
+console.log(`5^5 = ${potencia}`)
+console.log(`33 % 5 = ${modulo}`)
+
+var teste0 = '100' + 50; // 10050
+    teste1 = '100' - 50; // 50
+    teste2 = '100' * 50; // 5000
+    teste3 = 'Comprei 10' / 5; // NaN
+
+console.log(teste0)
+console.log(teste1)
+console.log(teste2)
+console.log(isNaN(teste3))
