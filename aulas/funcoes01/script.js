c60bdb18186896ca3f0f1715598eb5802d77c80a
Micael Araujo <micaelsilva.dev@gmail.com>
2026-02-14 17:58:03 -0300

    Funções

--- aulas/funcoes01/script.js
+++ aulas/funcoes01/script.js
@@ -1,5 +1,13 @@
-function areaQuadrado(lado) {
-  return lado * lado
+function imc(peso, altura) {
+  const imc = peso / (altura ** 2);
+  return imc;
 }
 
-console.log(areaQuadrado(2))
+console.log(imc(80, 1.80))
+console.log(imc(60, 1.71))
+
+function mostraConsole() {
+  console.log('Click')
+}
+
+addEventListener('click', mostraConsole);
