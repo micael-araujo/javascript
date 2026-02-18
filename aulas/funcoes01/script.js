613da02de1bf42a2d21e5616ae25eca77a4731da
Micael Araujo <micaelsilva.dev@gmail.com>
2026-02-18 17:36:55 -0300

    Mais funções

--- aulas/funcoes01/script.js
+++ aulas/funcoes01/script.js
@@ -3,11 +3,44 @@ function imc(peso, altura) {
   return imc;
 }
 
-console.log(imc(80, 1.80))
-console.log(imc(60, 1.71))
+console.log(imc(60, 1.70))
 
 function mostraConsole() {
   console.log('Click')
 }
 
 addEventListener('click', mostraConsole);
+
+function terceiraIdade(idade) {
+  if (typeof idade !== 'number') {
+    return 'Informe a sua idade';
+  } else if (idade >= 60) {
+    return true;
+  } else {
+    return false;
+  }
+} // Ideal retornar um tipo de dado, não vários como esta função
+
+console.log(terceiraIdade(60))
+
+function paisesVisitados(faltamVisitar) {
+  var totalPaises = 193;
+  console.log(`Faltam visitar ${totalPaises - faltamVisitar} países.`)
+}
+
+paisesVisitados(20)
+
+var profissao = 'Aprendiz';
+
+function dados() {
+  var nome = 'Micael';
+  var idade = 18;
+  function outrosDados() {
+    var endereco = 'Santa Catarina';
+    var idade = 19;
+    return `${nome}, ${idade}, ${endereco}, ${profissao}`
+  }
+  return outrosDados();
+}
+
+console.log(dados());
