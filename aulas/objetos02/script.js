58883e05143625e0291901b77733c960f1f7144e
Micael Araujo <micaelsilva.dev@gmail.com>
2026-02-20 19:34:35 -0300

    Exercicios objetos

---
+++ aulas/objetos02/script.js
@@ -0,0 +1,14 @@
+var height = 100;
+var menu = {
+  width: 800,
+  height: 50,
+  backgroundColor: '#84E',
+  metadeHeight() {
+    return this.height / 2;
+  }
+}
+
+menu.backgroundColor = '#000';
+menu.color = '#09A';
+
+var bg = menu.backgroundColor;
