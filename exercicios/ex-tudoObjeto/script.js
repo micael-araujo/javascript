7823c33f3b4b2979151d7a7148121e2f3e225d1d
Micael Araujo <micaelsilva.dev@gmail.com>
2026-02-24 20:32:27 -0300

    Exercícios de Tudo é Objeto

--- exercicios/ex-tudoObjeto/script.js
+++ exercicios/ex-tudoObjeto/script.js
@@ -1,6 +1,9 @@
 // nomeie 3 propriedades ou métodos de strings
+// R: length fixed slice
 
 // nomeie 5 propriedades ou métodos de elementos do DOM
+// R: addEventListener appendChild id innerHtml outerHtml
+
 
 // busque na web um objeto (método) capaz de interagir com o clipboard, 
 // clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
