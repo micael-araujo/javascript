10d48bacc60d66c5c12c28fae75333df80112492
Micael Araujo <micaelsilva.dev@gmail.com>
2026-02-27 19:35:24 -0300

    Tudo é Objeto

---
+++ aulas/tudo-e-objeto/script.js
@@ -0,0 +1,15 @@
+var videoGames = ['Switch', 'PS4', 'XBox'];
+
+var ultimoItem = videoGames.pop();
+
+console.log('For Loop');
+for (var numero = 0; numero <= 100; numero = numero + 10) {
+  console.log(numero);
+}
+
+console.log('While Loop');
+var i = 0;
+while (i <= 10) {
+ console.log(i);
+ i++;
+}
